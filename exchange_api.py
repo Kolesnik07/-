b228f7d7c92330b78e38e9dceed1524d8be3c5db
Kolesnik07 <malgina301207@gmail.com>
2026-05-23 18:04:16 +0300

    Update exchange_api.py

--- exchange_api.py
+++ exchange_api.py
@@ -1,22 +1,3 @@
-"""
-Модуль работы с API биржи
-"""
-
-import requests
-
-class ExchangeAPI:
-
-    def get_balance(self):
-        """
-        Получение баланса
-        """
-        return {
-            "USDT": 1000
-        }
-
-    def place_order(self, symbol, side, quantity):
-        """
-        Отправка ордера
-        """
-
-        print(f"Ордер отправлен: {side} {symbol} {quantity}")
+class RiskManager:
+    def calculate_position_size(self, balance, ri:
+                                return balance * risk_per_trade
