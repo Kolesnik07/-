25f215e8b63b96a456b7195b169f528922d53e85
Kolesnik07 <malgina301207@gmail.com>
2026-05-23 18:01:26 +0300

    Update and rename logger.py to core.py

---
+++ core.py
@@ -0,0 +1,7 @@
+class StrategyCore:
+    def generate_signal(self, data) :
+        if datalma shortil › datal'ma lonoll
+            return "BUY"
+        elif datal 'ma_short'] ‹ datal 'ma_long']:
+            return "SELL"
+        return "HOLD" 
